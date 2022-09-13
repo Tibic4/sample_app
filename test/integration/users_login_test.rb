a87e14f1b78f177db6eeda26218f1d36080b0725
Alton Vieira <bicagold@gmail.com>
2022-09-13 09:19:04 -0300

    end basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,23 +1,44 @@
 require 'test_helper'
-class UsersLoginTest < ActionDispatch::IntegrationTest
+
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
+end
+
+class InvalidPasswordTest < UsersLogin
+  test 'login path' do
+    get login_path
+    assert_template 'sessions/new'
+  end
 
   test 'login with valid email/invalid password' do
-    # Login get assertion session new
-    get login_path
-    post login_path, params: { session: { email: @user.email, password: 'invalid' } }
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'invalid' } }
+    assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
+end
 
-  test 'login with valid information' do
-    post login_path, params: { session: { email: @user.email, password: 'password' } }
+class ValidLogin < UsersLogin
+  def setup
+    super
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+  test 'valid login' do
+    assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test 'redirect after login' do
     follow_redirect!
     assert_template 'users/show'
     assert_select 'a[href=?]', login_path, count: 0
